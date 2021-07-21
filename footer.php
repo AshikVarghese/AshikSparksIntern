6cbc0ff64175133906e0c12815cb0f1fda68fd83
AshikVarghese <67852484+AshikVarghese@users.noreply.github.com>
2021-07-21 16:39:23 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
         <div class="navbar-brand center">
-          <h5 style="margin-top:0px; ">Internship project by Elizabeth Subhiksha Victoria @ The Sparks Foundation</h5>
+          <h5 style="margin-top:0px; ">Internship project by Ashik Varghese @ The Sparks Foundation</h5>
         </div>
       </div> <!-- .navbar-header -->
     </div> <!-- .container -->
