b87f8f4e023471a5e1826a49ec787ae094d42d00
AshikVarghese <67852484+AshikVarghese@users.noreply.github.com>
2021-07-21 20:28:51 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "internship";
+$db_user = "id17285722_root";
+$db_pass = "|3^eaWlN)iZoSS<v";
+$db_name = "id17285722_internship";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
